ab95e347ee66261527bb2f77e733a5a4f3f3a536
work-kcw <work@kcwesley.uk>
2020-02-22 14:34:32 +0000

    custom airtable plugin

--- gridsome.config.js
+++ gridsome.config.js
@@ -1,10 +1,15 @@
-// This is where project configuration and plugin options are located. 
-// Learn more: https://gridsome.org/docs/config
-
 // Changes here require a server restart.
 // To restart press CTRL + C in terminal and run `gridsome develop`
 
 module.exports = {
-  siteName: 'Gridsome',
-  plugins: []
+  siteName: 'cn-proto',
+  plugins: [
+    {
+      use: '~/src/sources/products',
+      options: {
+        apiKey: 'keyrBK5AQ6XDpRwPw',
+        base: 'appJZsfZmYaICRUig'
+      }
+    }
+  ]
 }
